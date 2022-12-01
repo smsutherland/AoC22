9e5c227b8f6fc3191f5e18401a2c86d1976340b1
smsutherland <sagan@farthinghalearms.com>
2022-12-01 12:20:30 -0500

    clean day 1

--- day1.nim
+++ day1.nim
@@ -1,17 +1,17 @@
 from std/parseutils import parseInt
-import std/algorithm
+from std/algorithm import sort, Descending
 
-var elves: seq[int] = @[0]
+var elves = @[0]
 var input = open("in/input1")
 var line: string
 while input.readLine(line):
   if line == "":
-    add(elves, 0)
+    elves.add(0)
   else:
     var num: int
-    discard parseInt(line, num, 0)
-    elves[len(elves) - 1] += num
-sort(elves, Descending)
+    discard line.parseInt(num, 0)
+    elves[elves.len() - 1] += num
+elves.sort(Descending)
 echo "Part 1: ", elves[0]
 echo "Part 2: ", elves[0] + elves[1] + elves[2]
 close(input)
